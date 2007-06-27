8a06eeb6c654c6f894419ebe1a9a3759fa79004a
edysegura <edysegura@02453509-d432-0410-a818-ffd0024ed06f>
2007-06-27 03:01:33 +0000

    Adição do componente Drag.js

---
+++ External/Drag/js/Index.js
@@ -0,0 +1,31 @@
+/* 
+ * Index.js
+ *
+ * Exemplo do uso do componente Drag.js
+ * @author: Edy Segura - infoedy@gmail.com
+ *
+ */
+
+var Index = {
+
+	init: function() {
+		Index.setDomDrag();
+	},//fim init
+
+
+	setDomDrag: function() {
+		var oDiv = document.getElementById("drag");
+		var oH3  = oDiv.getElementsByTagName('h3')[0];
+
+		oDiv.style.top  = "155px";
+		oDiv.style.left = "155px";
+
+		//usando componente Drag
+		Drag.init(oH3, oDiv);
+
+	}//fim setDomDrag
+
+};//fim Index
+
+//inicialização
+window.onload = Index.init;
