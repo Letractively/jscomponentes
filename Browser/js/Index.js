bf3e13c9083f1a2cbb5e92656e749d9567a65211
edysegura <edysegura@02453509-d432-0410-a818-ffd0024ed06f>
2007-07-06 04:55:29 +0000

    Atualização do objeto Browser.js

--- Browser/js/Index.js
+++ Browser/js/Index.js
@@ -1,7 +1,7 @@
 /* 
  * Index.js
  *
- * Objeto Literal Index
+ * Objeto Literal Index para uso do objeto Browser.js
  * @author: Edy Segura - infoedy@gmail.com
  *
  */
@@ -10,7 +10,32 @@ var Index = {
 	
 	init: function() {
 		Index.buildTable();
+		Index.createButtons();
 	},//fim init
+	
+	
+	createButtons: function() {
+		var oButton1 = document.createElement('button');
+		var oButton2 = document.createElement('button');
+		
+		oButton1.onclick = function() {
+			if(Browser && Browser.addBookmark) {
+				Browser.addBookmark("http://edysegura.com", "Edy Segura");
+			}
+		}
+		
+		oButton2.onclick = function() {
+			if(Browser && Browser.installSearchEngine) {
+				Browser.installSearchEngine("http://edysegura.com/xml/opensearch.xml");
+			}
+		}
+		
+		oButton1.appendChild(document.createTextNode('Adicionar ao favoritos'));
+		oButton2.appendChild(document.createTextNode('Adicionar search engine'));
+		
+		document.body.appendChild(oButton1);
+		document.body.appendChild(oButton2);
+	},//fim createButton
 	
 	
 	buildTable: function() {
@@ -19,6 +44,8 @@ var Index = {
 		
 		//percorrendo o objeto Browser
 		for(var sProperty in Browser) {
+			if(/addBookmark|installSearchEngine/.test(sProperty)) continue;
+			
 			var oRow          = oTbody.insertRow(oTbody.rows.length);
 			var oCellProperty = oRow.insertCell(oRow.cells.length);
 			var oCellValue    = oRow.insertCell(oRow.cells.length);
@@ -31,7 +58,7 @@ var Index = {
 		
 	}//fim buildTable
 	
-};//fim Index.js
+};//fim Index
 
 //inicializacao
 window.onload = Index.init;
