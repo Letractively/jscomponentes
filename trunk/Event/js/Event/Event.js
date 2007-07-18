f1f0731285397dd616c465f62ef3be74a02f0dec
edysegura <edysegura@02453509-d432-0410-a818-ffd0024ed06f>
2007-07-18 03:06:16 +0000

    Atualização no padrão do charset de utf-8 para UTF-8

--- trunk/Event/js/Event/Event.js
+++ trunk/Event/js/Event/Event.js
@@ -5,11 +5,6 @@
  * Objeto Literal Event. Documentacao completa disponivel em: 
  * http://code.google.com/p/jscomponentes/wiki/Event
  *
- * Referencia: 1 - http://ejohn.org/projects/flexible-javascript-events/
- * Referencia: 2 - http://www.quirksmode.org/blog/archives/2005/10/_and_the_winner_1.html
- * Referencia: 3 - Livro Professional JavaScript for Web Developer página 292 do livro e 320 do PDF
- * Referencia: 4 - http://www.jsfromhell.com/geral/event-listener
- *
  */
 
 var Event = {
@@ -29,22 +24,8 @@ var Event = {
 	},
 	
 	
-	//remover evento dinamicamente
-	removeHandle: function(oElement, sType, oFunction) {
-		if(oElement.detachEvent) {
-			oElement.detachEvent('on' + sType, oElement[sType + oFunction]);
-			oElement[sType + oFunction] = null;
-		}
-		else if(oElement.addEventListener) {
-			oElement.removeEventListener(sType, oFunction, false);
-			return true;
-		}
-		else return false;
-	},
-
-
 	//adicionar evento dinamicamente, http://www.jsfromhell.com/geral/event-listener
-	addEvent: function(o, e, f, s){
+	addEvent: function(o, e, f, s) {
 		var r = o[r = "_" + (e = "on" + e)] = o[r] || (o[e] ? [[o[e], o]] : []), a, c, d;
 		r[r.length] = [f, s || o], o[e] = function(e){
 			try{
@@ -59,8 +40,27 @@ var Event = {
 	},
 	
 	
+	addLoad: function(oFunction) {
+		Event.addHandle(window, 'load', oFunction);
+	},//fim addLoad
+	
+	
+	//remover evento dinamicamente
+	removeHandle: function(oElement, sType, oFunction) {
+		if(oElement.detachEvent) {
+			oElement.detachEvent('on' + sType, oElement[sType + oFunction]);
+			oElement[sType + oFunction] = null;
+		}
+		else if(oElement.addEventListener) {
+			oElement.removeEventListener(sType, oFunction, false);
+			return true;
+		}
+		else return false;
+	},
+	
+	
 	//remover evento dinamicamente, http://www.jsfromhell.com/geral/event-listener
-	removeEvent: function(o, e, f, s){
+	removeEvent: function(o, e, f, s) {
 		for(var i = (e = o["_on" + e] || []).length; i;)
 			if(e[--i] && e[i][0] == f && (s || o) == e[i][1])
 				return delete e[i];
