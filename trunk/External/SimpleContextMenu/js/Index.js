539626c744db3c61560575212bd39eff9e87b29f
edysegura <edysegura@02453509-d432-0410-a818-ffd0024ed06f>
2007-12-14 16:34:50 +0000

    Adição do SimpleContextMenu para IE e FF

--- trunk/External/SimpleContextMenu/js/Index.js
+++ trunk/External/SimpleContextMenu/js/Index.js
@@ -9,11 +9,32 @@ var Index = {
 	
 	init: function() {
 		Index.setContextMenu();
+		Index.setMouseUp();
 	},
 	
+	
 	setContextMenu: function() {
-		SimpleContextMenu.setup({'preventDefault':false, 'preventForms':false});
+		SimpleContextMenu.setup({'preventDefault':true, 'preventForms':false});
 		SimpleContextMenu.attach('container', 'CM1'); //className and id of the contextmenu
+	},
+	
+	
+	setMouseUp: function() {
+		var aDivs = document.getElementsByTagName('div');
+		
+		for(var i=0; i<aDivs.length; i++) {
+			aDivs[i].onmouseup = Index.showTarget;
+		}
+	},
+	
+	
+	showTarget: function(e) {
+		e = e ? e : window.event;
+		if(e.button == 2) {
+			var oInput = document.getElementById('field');
+			var oElement = e.srcElement || e.target;
+			oInput.value = oElement.innerHTML;
+		}
 	}
 	
 }
