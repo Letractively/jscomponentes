393a9419cde650f38a131d48527c99b9c3aef847
edysegura <edysegura@02453509-d432-0410-a818-ffd0024ed06f>
2008-03-26 17:46:30 +0000

    Adição de uma página index.html tem todos os diretórios dos componentes.

---
+++ trunk/Common/js/Index.js
@@ -0,0 +1,16 @@
+﻿/**
+ *
+ * Objeto Literal Index
+ * @author: Edy Segura - edy@segura.eti.br
+ *
+ */
+
+var Index = {
+	
+	init: function() {
+	}
+	
+};
+
+//inicializacao
+window.onload = Index.init;
