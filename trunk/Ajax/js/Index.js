6ffe176e8bdbee27e36411c0b5cc21658879d326
edysegura <edysegura@02453509-d432-0410-a818-ffd0024ed06f>
2008-04-01 14:21:04 +0000

    Atualização do Componente Ajax.js

--- trunk/Ajax/js/Index.js
+++ trunk/Ajax/js/Index.js
@@ -1,6 +1,6 @@
 ﻿/**
  *
- * Objeto Literal Index
+ * Objeto Literal Index para uso do componente Ajax.js
  * @author: Edy Segura - edy@segura.eti.br
  *
  */
@@ -35,39 +35,39 @@ var Index = {
 	
 
 	setLinkClear: function() {
-		var oLink = document.links[2];
+		var link = document.links[2];
 		
-		oLink.onclick = function() {
+		link.onclick = function() {
 			document.getElementById('content-1').innerHTML = 
 			document.getElementById('content-2').innerHTML = "";
 			return false;
 		};
-
-	},//fim setLinkClear
+		
+	},
 	
 	
-	showContent: function(sResponse, oTarget) {
-		oTarget.innerHTML = sResponse;
-	},//fim showContent
+	showContent: function(response, target) {
+		target.innerHTML = response;
+	},
 	
 	
-	showContent1: function(sResponse) {
-		var oTarget = document.getElementById('content-1');
-		Index.showContent(sResponse, oTarget);
-	},//fim showContent1
+	showContent1: function(response) {
+		var target = document.getElementById('content-1');
+		Index.showContent(response, target);
+	},
 	
 	
-	showContent2: function(sResponse) {
-		var oTarget = document.getElementById('content-2');
-		Index.showContent(sResponse, oTarget);
-	},//fim showContent2
+	showContent2: function(response) {
+		var target = document.getElementById('content-2');
+		Index.showContent(response, target);
+	},
 	
 	
 	showMsg: function() {
 		alert("Componente Ajax.js");
 	}
 	
-};//fim Index
+};
 
-//inicialização
+//inicializacao
 window.onload = Index.init;
