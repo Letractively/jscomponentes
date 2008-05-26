d92f7d5d34bdb473be9d82ae794b528a5d2ca374
edysegura <edysegura@02453509-d432-0410-a818-ffd0024ed06f>
2008-05-26 14:04:10 +0000

    Atualização do componente Table.js

--- Table/js/Index.js
+++ Table/js/Index.js
@@ -1,6 +1,6 @@
 ﻿/**
  * 
- * Script para uso do componente Table.js
+ * Exemplo de uso do componente Table.js
  * @author: Edy Segura - edy@segura.eti.br
  *
  */
@@ -8,21 +8,14 @@
 var Index = {
 	
 	init: function() {
-		Index.setTableAttribute();
 		Index.setStripedTable();
-	},
-	
-	
-	setTableAttribute: function() {
-		var oTable = document.getElementById("zebratable");
-		oTable.setAttribute("cellSpacing", "1");
 	},
 	
 	
 	setStripedTable: function() {
 		Table.stripedTable(document.getElementById("zebratable"));
 		Table.stripedTable("simpletable", "alternada");
-		//Table.stripedTable('nada');
+		//Table.stripedTable("nada");
 	}
 
 };
