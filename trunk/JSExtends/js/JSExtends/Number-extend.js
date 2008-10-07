489b13b95ebecb6aece7db11398873abac44f365
edysegura <edysegura@02453509-d432-0410-a818-ffd0024ed06f>
2008-10-07 20:27:42 +0000

    Atualização e adição do método zeroFormat

--- trunk/JSExtends/js/JSExtends/Number-extend.js
+++ trunk/JSExtends/js/JSExtends/Number-extend.js
@@ -1,5 +1,5 @@
-﻿/** 
- * 
+﻿/**
+ *
  * Extendendo o objeto Number do javascript
  *
  * Number-extends.js
@@ -8,6 +8,7 @@
  * @author: Edy Segura - edy@segura.pro.br
  *
  */
+
 
 /**
  *
@@ -20,6 +21,7 @@ Number.prototype.isInt = function() {
  if(isNaN(value)) return false;
  return (this == value && this.toString() == value.toString());
 }
+
 
 /**
  *
@@ -39,26 +41,46 @@ Number.prototype.numberFormat = function(fractionNumber, fractionSeparator, thou
   var fractionNumber    = fractionNumber    || 0;
   var fractionSeparator = fractionSeparator || ".";
   var thousandSeparator = thousandSeparator || ",";
-	
+
   if((typeof fractionNumber != "number")
     || (typeof fractionSeparator != "string")
     || (typeof thousandSeparator != "string")) {
     throw new Error("Wrong parameters for Number.numberFormat() method.");
   }
-	
+
   var integer = "", decimal = "";
   var number = new String(this).split(/\./), numberLength = number[0].length, i = 0;
-  
+
 	if (fractionNumber > 0) {
     number[1] = (typeof number[1] != "undefined") ? number[1].substr(0, fractionNumber) : "";
     decimal = fractionSeparator.concat(number[1].pad(fractionNumber, "0", String.PAD_RIGHT));
   }
+
   while (numberLength > 0) {
     if ((++i % 3 == 1) && (numberLength != number[0].length)) {
       integer = thousandSeparator.concat(integer);
     }
     integer = number[0].substr(--numberLength, 1).concat(integer);
   }
-	
+
   return (integer + decimal);
 }
+
+
+/**
+ *
+ * Adiciona zeros à esquerda ou à direita em um número
+ * Number.zeroFormat(n: Integer, [fill: Boolean = false], [right: Boolean = false]): String
+ *
+ * var N = 123;
+ * N.zeroFormat(5, true, true) = 12300
+ * N.zeroFormat(5, true) = 00123
+ * N.zeroFormat(5) = 00000123
+ *
+ * http://jsfromhell.com/number/zero-format
+ * @author Carlos R. L. Rodrigues, editador por Edy Segura - edy@segura.pro.br
+ *
+ */
+Number.prototype.zeroFormat = function(n, f, r){
+    return n = new Array((++n, f ? (f = (this + "").length) < n ? n - f : 0 : n)).join(0), r ? this + n : n + this;
+};
