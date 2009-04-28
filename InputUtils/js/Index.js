66d49527845f8fa0b26550c3002e3bfa5a1f10c0
edysegura <edysegura@02453509-d432-0410-a818-ffd0024ed06f>
2009-04-28 18:34:49 +0000

    Atualização do componente InputUtils.js

--- InputUtils/js/Index.js
+++ InputUtils/js/Index.js
@@ -4,16 +4,29 @@
  * @author: Edy Segura - edy@segura.pro.br
  *
  */
-
 var Index = {
 	
 	init: function() {
+		Index.setForm();
 		Index.setInputs();
 	},
 	
 	
+	setForm: function() {
+		var form = document.forms[0];
+		if(form) {
+			form.onsubmit = function() {
+				return false;
+			}
+		}
+	},
+	
+	
 	setInputs: function() {
-		
+		var inputLetter = document.getElementById('letter');
+		var inputNumber = document.getElementById('number');
+		InputUtils.setLetterOnly([inputLetter]);
+		InputUtils.setNumberOnly([inputNumber]);
 	}
 	
 };
