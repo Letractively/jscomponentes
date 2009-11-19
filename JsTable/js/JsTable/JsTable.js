7d3a68e76b314d949fd4c78edf645b3bda68f733
edysegura <edysegura@02453509-d432-0410-a818-ffd0024ed06f>
2009-11-19 20:20:50 +0000

    - Atualização do componente JSTable

--- JsTable/js/JsTable/JsTable.js
+++ JsTable/js/JsTable/JsTable.js
@@ -5,6 +5,9 @@
  */
 var JsTable = function(params) {
 
+	//atributo privado
+	var jsTable = this;
+	
 	//atributos
 	this.tableId      = (params.tableId) ? params.tableId : null;
 	this.containerId  = (params.containerId) ? params.containerId : null;
@@ -15,10 +18,10 @@ var JsTable = function(params) {
 	
 	//atributos para paginação
 	this.itemsPerPage = (params.itemsPerPage) ? params.itemsPerPage : 10;
-	this.totalPages   = 0;
-	this.currentPage  = 1;
 	
 	if(this.pagingId) {
+		this.currentPage = 1;
+		this.totalPages  = 0;
 		this.pagingContainer = document.getElementById(this.pagingId);
 		this.showTotalPage = $('.totalPages', this.pagingContainer).get(0);
 		this.showTotalItems = $('.totalItems', this.pagingContainer).get(0);
@@ -26,6 +29,7 @@ var JsTable = function(params) {
 		this.inputItemsPerPage = $('.itemsPerPage', this.pagingContainer).get(0);
 	}	
 	
+	//tratamento de erros
 	if(!this.tableId) {
 		this.errors.push("tableId is not defined!");
 	}
@@ -74,9 +78,11 @@ var JsTable = function(params) {
 					+ this.getFooter()
 					+ this.getBody()
 			+ '</table>';
+			
 			$('#' + this.containerId).html(tableString);
+			
 			if(this.pagingId) {
-				this.setPagination(this.itemsPerPage);
+				this.initPaging();
 			}
 		}
 	};
@@ -170,15 +176,20 @@ var JsTable = function(params) {
 	
 	//--------- métodos para paginação ------------------------------------
 	
-	this.setPagination = function(itemsPerPage) {
+	this.initPaging = function() {
+		this.setPaging(this.itemsPerPage);
+		this.setPagingControls();
+		this.showTotalItens();
+	};
+	
+	this.setPaging = function(itemsPerPage) {
 		this.itemsPerPage = itemsPerPage;
 		var totalPages = Math.ceil(this.model.getNumRows() / this.itemsPerPage);
 		if(totalPages === 0) {
 			totalPages = 1;
 		}
 		this.totalPages = totalPages;
-		this.setStatusPagination();
-		this.showTotalItens();
+		this.setStatusPaging();
 	};
 	
 	this.showTotalItens = function() {
@@ -187,16 +198,122 @@ var JsTable = function(params) {
 		}
 	};
 	
-	this.setStatusPagination = function() {
+	this.setStatusPaging = function() {
 		if(this.showTotalPage) {
 			this.showTotalPage.innerHTML = "/" + this.totalPages;
 		}
 		if(this.currentPage) {
-			this.currentPage.value = this.currentPage;
+			this.currentPage.value =
+			this.currentPage.defaultValue = this.currentPage;
 		}
 	};
 	
-	this.setControllsPagination = function() {
+	this.setPagingControls = function() {
+		var divPagination = this.pagingContainer,
+		    begin = 0;
+		
+		//ir para primeira página
+		$('.first', divPagination).click(function() {
+			if(jsTable.currentPage > 1) {
+				jsTable.currentPage = 1;
+				jsTable.showPagingRows(0, jsTable.itemsPerPage);
+				jsTable.setStatusPaging();
+			}
+		});
+		
+		//ir para página anterior
+		$('.prev', divPagination).click(function() {
+			if(jsTable.currentPage > 1) {
+				jsTable.currentPage--;
+				begin = (jsTable.currentPage-1) * jsTable.itemsPerPage;
+				jsTable.showPagingRows(begin, jsTable.itemsPerPage);
+				jsTable.setStatusPaging();
+			}
+		});
+		
+		//ir para próxima página
+		$('.next', divPagination).click(function() {
+			if(jsTable.currentPage < jsTable.totalPages) {
+				jsTable.currentPage++;
+				begin = (jsTable.currentPage-1) * jsTable.itemsPerPage;
+				jsTable.showPagingRows(begin, jsTable.itemsPerPage);
+				jsTable.setStatusPaging();
+			}
+		});
+		
+		//ir para última página
+		$('.last', divPagination).click(function() {
+			if(jsTable.currentPage < jsTable.totalPages) {
+				jsTable.currentPage = jsTable.totalPages;
+				begin = (jsTable.currentPage-1) * jsTable.itemsPerPage;
+				jsTable.showPagingRows(begin, jsTable.itemsPerPage);
+				jsTable.setStatusPaging();
+			}
+		});
+		
+		//ir para página informada
+		$(this.inputCurrentPage).keydown(function(e) {
+			if(e.keyCode == 13) {
+				var numPage = 1;
+				
+				if(this.value != "") {
+					numPage = parseInt(this.value);
+				}
+				if(numPage > jsTable.totalPages) {
+					numPage = jsTable.totalPages;
+				} 
+				else if(numPage <= 0) {
+					numPage = 1;
+				}
+				
+				this.value = numPage;			
+				jsTable.currentPage = numPage;
+				begin = (jsTable.currentPage-1) * jsTable.itemsPerPage;
+				jsTable.showPagingRows(begin, jsTable.itemsPerPage);
+				jsTable.setStatusPaging();
+			}
+		});
+		
+		//lista número de itens informado por página
+		$(this.inputItemsPerPage).keydown(function(e) {
+			if(e.keyCode == 13) {
+				var numItens = 1;
+				
+				if(this.value != "") {
+					numItens = parseInt(this.value);
+				}
+				
+				if(numItens > 100) {
+					numItens = 100;
+				} 
+				else if(numItens <= 0) {
+					numItens = 1;
+				}
+				
+				this.value = numItens;
+				
+				jsTable.setPaging(numItens);
+				jsTable.showPagingRows(0, jsTable.itemsPerPage);
+				jsTable.currentPage = 1;
+				jsTable.setStatusPaging();
+			}
+		});
+	};
+	
+	this.showPagingRows = function (begin, n) {
+		if ( this.model.getNumRows() ) {
+			var table  = document.getElementById(this.tableId),
+			    rowStr = "", 
+					tbody  = table.getElementsByTagName('tbody')[0];
+			
+			$(tbody).html("");
+			
+			for(var index=begin, i=0; index<this.model.getNumRows() && i<n; index++, i++) {
+				rowStr += this.rowRenderer(i);
+				$(tbody).append(rowStr);
+			}
+			
+		}
 	};
 
 }
