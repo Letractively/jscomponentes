aee9237b01b91376b5c6f8cbaa22fbd5aed865df
edysegura <edysegura@02453509-d432-0410-a818-ffd0024ed06f>
2009-11-27 17:52:20 +0000

    - Modularização para adicionar vários critérios para sorter

--- trunk/JsTable/js/JsTable/JsSorter.js
+++ trunk/JsTable/js/JsTable/JsSorter.js
@@ -7,8 +7,8 @@ var JsSorter = {
 	sortByNumber: function(columnName, columnNumber, model, factor) {
 		var rawData = model.getRawData(), result, valor1, valor2;
 		rawData.sort(function(a, b) {
-			valor1 = JsSorter.parseNumber(a[columnName]);
-			valor2 = JsSorter.parseNumber(b[columnName]);
+			valor1 = JsSorter.parseNumber(a[columnNumber] || a[columnName]);
+			valor2 = JsSorter.parseNumber(b[columnNumber] || b[columnName]);
 			result = valor1 - valor2;
 			return result * factor;
 		});
@@ -18,7 +18,7 @@ var JsSorter = {
 		var number;
 		number = valor.replace(/\./g, "");
 		number = number.replace(/\,/g, ".");
-		number = number.replace(/[^0-9.]/g, "");
+		number = number.replace(/[^0-9.-]/g, "");
 		number = parseFloat(number);
 		return number;
 	}
