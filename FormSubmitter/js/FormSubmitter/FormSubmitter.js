05ff62d76a335e3f442ce8f4d3566ac10af96ad7
edysegura <edysegura@02453509-d432-0410-a818-ffd0024ed06f>
2010-02-26 13:19:29 +0000

    - Atualização do componente FormSubmitter.js

--- FormSubmitter/js/FormSubmitter/FormSubmitter.js
+++ FormSubmitter/js/FormSubmitter/FormSubmitter.js
@@ -6,14 +6,18 @@
  */
 var FormSubmitter = {
 	
-	callbackSuccess: null,
-	callbackError: null,
+	CALLBACK_SUCCESS: 1,
+	CALLBACK_ERROR: 2,
+	
+	callbacks: [],
 	
 	init: function(form, callSuccess, callError) {
+		if(form && (callSuccess || callError)) {
+			FormSubmitter.callbacks.push([form, callSuccess, callError]);
+		}
 		FormSubmitter.setForm(form);
-		FormSubmitter.callbackSuccess = callSuccess;
-		FormSubmitter.callbackError   = callError;
 	},
+	
 	
 	setForm: function(form) {
 		if(form) {
@@ -21,40 +25,77 @@ var FormSubmitter = {
 		}
 	},
 	
+	
 	submitListener: function() {
 		var form = this,
 		    data = $j(form).serialize();
-
+		
+		//for jquery.validate
+		//if(!$j(form).validate().form()) return false;
+		
 		$j.ajax({
 		  type    : "POST",
 		  url     : form.action,
 		  data    : data,
-		  success : FormSubmitter.responseListener,
-		  error   : FormSubmitter.errorAjax
+		  error   : FormSubmitter.errorAjax,
+		  success : function(data){
+		  	FormSubmitter.responseListener(data, form);
+		  }
 	 	});
 	 		 
 		return false;
 	},
 	
+	
 	errorAjax: function(xhr, errorMessage, errorThrows) {
 		alert("Error: " + errorMessage + " " + errorThrows);
 	},
 	
-	responseListener: function(response) {
-		response = eval ("("+response+")");
-		alert(response.message);
+	
+	responseListener: function(response, form) {
+		try {
+			response = eval ("(" + response + ")");
+			alert(response.message);
+		}
+		catch(e) {
+			response = {status:'ERROR', message: e.message};
+		}
+		
 		if(response.status == "SUCCESS") {
-			if(FormSubmitter.callbackSuccess) {
-				FormSubmitter.callbackSuccess(response);
-			}
-			FormSubmitter.formFilterSubmit();
+			FormSubmitter.callForeignCallback (
+				response,
+				form,
+				FormSubmitter.CALLBACK_SUCCESS
+			);
 		}
 		else if(response.status == "ERROR") {
-			if(FormSubmitter.callbackError) {
-				FormSubmitter.callbackError(response);
-			}
+			FormSubmitter.callForeignCallback (
+				response,
+				form,
+				FormSubmitter.CALLBACK_ERROR
+			);
 		}
 	},
+	
+	
+	callForeignCallback: function(response, form, callbackType) {
+		var callbacks = FormSubmitter.callbacks,
+		    params = [],
+		    continueSubmit = true;
+		
+		for(var i in callbacks) {
+			params = callbacks[i];
+			
+			if(params[0] === form && params[callbackType]) {
+				continueSubmit = params[callbackType](response, form);
+			}
+		}
+		
+		if(continueSubmit) {
+			FormSubmitter.formFilterSubmit();
+		}
+	},
+	
 	
 	formFilterSubmit: function() {
 		var formFiltro = document.getElementById('form-filtro');
