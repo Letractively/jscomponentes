3d7d28fcaacf00093500ca34f8e0ed5bd38cef49
edysegura <edysegura@02453509-d432-0410-a818-ffd0024ed06f>
2010-04-15 13:39:33 +0000

    - Atualização do componente TableDnD

--- trunk/TableDnD/js/Index.js
+++ trunk/TableDnD/js/Index.js
@@ -7,7 +7,11 @@ var Index = {
 		TableActions.init({
 			tableId: 'list-ramal',
 			onDragListener: Index.showLinhaSelecionada,
-			onDropListener: Index.showNewOrder
+			onDropListener: Index.showNewOrder,
+			cmenuListeners: {
+				'menu-1': Index.addNewLine,
+				'menu-2': Index.duplicateLine
+			}
 		});
 		
 	},
@@ -34,6 +38,45 @@ var Index = {
 		console += "Nova ordem: " + rowsId.join(", ");
 		
 		$('#console').html(console);
+	},
+	
+	
+	addNewLine: function(row) {
+		var newRow = jQuery(row).clone(true).get(0);
+		
+		newRow.id = Index.generateId();
+		jQuery('th, td', newRow).html('&nbsp;');
+		jQuery(newRow).insertAfter(row);
+		
+		Index.showTarget(row);
+	},
+	
+	
+	duplicateLine: function(row) {
+		var newRow = jQuery(row).clone(true).get(0);
+		
+		newRow.id = Index.generateId();
+		jQuery(newRow).insertAfter(row);
+		
+		Index.showTarget(row);
+	},
+	
+	
+	showTarget: function(row) {
+		var debug = [], rows = jQuery('#list-ramal tbody tr').get();
+		debug.push("RowId: " + row.id);
+		
+		if(rows[row.rowIndex]) {
+			debug.push("NewRowId: " + rows[row.rowIndex].id);
+		}
+		
+		alert(debug.join("\n"));
+	},
+	
+	
+	generateId: function() {
+		var numRows = jQuery('#list-ramal tbody tr').length;
+		return "rm-" + (numRows + 100);
 	}
 	
 };
